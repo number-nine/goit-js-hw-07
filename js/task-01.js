9ec7a721a2b6523918a63924d2639924da51b240
number-nine <sergii.pokhylko@gmail.com>
2023-03-30 23:43:39 +0300

    initial

--- js/task-01.js
+++
@@ -1,8 +0,0 @@
-const liListRef = document.querySelectorAll('li.item');
-
-console.log('Number of categories: ', liListRef.length);
-
-liListRef.forEach(element => {
-    console.log('Category: ', element.firstElementChild.textContent);
-    console.log('Elements: ', element.lastElementChild.children.length);
-});
