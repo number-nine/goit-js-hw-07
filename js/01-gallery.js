256f30a6df5ca7034a4329bedf3807726baba09f
number-nine <sergii.pokhylko@gmail.com>
2023-04-01 08:57:45 +0300

    task 1 without lazyload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// import { basicLightbox } from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
+
 initGalleryScript();
-// const basicLightbox = require('basiclightbox');
-// import * as basicLightbox from 'basiclightbox';
-
-
-// console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
+createGalleryMarkup(galleryItems, galleryContainer);
+galleryContainer.addEventListener('click', onGalleryClick);
+
+function initGalleryScript() {
+  const scriptCDN = document.createElement('script');
+  scriptCDN.src =
+    'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
+  document.body.append(scriptCDN);
+  document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css">'
+  );
+}
+
+function onGalleryClick(evt) {
+  const controller = new AbortController();
+  const currentElement = evt.target;
+
+  evt.preventDefault();
+
+  if (!currentElement.classList.contains('gallery__image')) {
+    return;
+  }
+  basicLightbox
+    .create(
+      composeFullscreenImgElement(currentElement.dataset.source, currentElement.alt),
+      {
+        onShow: (inst, ctrl = controller) => {
+          onFullscreenImageShow(inst, ctrl)
+        },
+        onClose: (inst, ctrl = controller) => {
+          ctrl.abort();
+        },
+      }
+    )
+    .show();
+}
+
+function onFullscreenImageShow(inst, ctrl){
+  document.body.addEventListener(
+    'keydown',
+    evt => {
+      if (!(evt.code === 'Escape')) {
+        return;
+      }
+      inst.close();
+    },
+    { signal: ctrl.signal }
+  );
+}
 
 function createGalleryMarkup(galleryItems, galleryContainer) {
   const galleryContent = galleryItems
@@ -28,47 +73,9 @@ function createGalleryMarkup(galleryItems, galleryContainer) {
   galleryContainer.insertAdjacentHTML('beforeend', galleryContent);
 }
 
-createGalleryMarkup(galleryItems, galleryContainer);
-
-
-galleryContainer.addEventListener('click', onGalleryClick);
-
-function onGalleryClick(evt) {
-    evt.preventDefault();
-    // console.dir(evt.target.dataStore);
-    if (!evt.target.classList.contains('gallery__image')) {
-        return;
-    };
-
-    // console.log(evt.target);
-
-    const bigImage = evt.target;
-    bigImage.src = evt.target.dataset.source;
-    const imageInstance = basicLightbox.create(
-      bigImage
-      //     `
-      //     <img
-      //         class="gallery__image"
-      //         src="${evt.target.dataset.source}"
-
-      //     />
-      // `
-    );
-    imageInstance.show();
-  //   console.log(evt);
+function composeFullscreenImgElement(original, description) {
+  return `<img
+            src="${original}"
+            alt="${description}"
+          />`;
 }
-
-function initGalleryScript() {
-  const scriptCDN = document.createElement('script');
-  scriptCDN.src =
-    'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
-    document.body.append(scriptCDN);
-    console.log('script injected')
-  document.head.insertAdjacentHTML(
-    'beforeend',
-    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css">'
-    );
-    console.log('css injected');
-}
-
-
